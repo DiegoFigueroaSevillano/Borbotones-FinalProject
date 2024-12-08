852f07626f1567ac2fb0a7f679e4ad1ad4ade5f1
DiegoFigueroaSevillano <diego.figueroa.8032745@usalesiana.edu.bo>
2024-12-08 16:17:58 -0400

    feat: client socket

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,4 +1,25 @@
 package Client;
 
+
+import Client.SocketManagement.SocketHandler;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8080;
+
+        try {
+            Socket clientSocket = new Socket(host, port);
+            SocketHandler socketHandler = new SocketHandler(clientSocket);
+            socketHandler.send("Hello from Client!");
+            String response = socketHandler.listen();
+            System.out.println("Server response: " + response);
+            socketHandler.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
