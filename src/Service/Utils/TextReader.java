2b428c628094aa682c21d2d923785529de2c0ea0
DiegoFigueroaSevillano <diego.figueroa.8032745@usalesiana.edu.bo>
2024-12-08 14:22:33 -0400

    feat: text comparator main test

---
+++ src/Service/Utils/TextReader.java
@@ -0,0 +1,16 @@
+package Service.View;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TextReader {
+
+    public String readTextFromFile(String absolutePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(absolutePath)));
+        } catch (IOException e) {
+            return "Error leyendo el archivo: " + e.getMessage();
+        }
+    }
+}
